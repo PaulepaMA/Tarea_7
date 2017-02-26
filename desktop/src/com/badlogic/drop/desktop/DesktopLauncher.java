fb974e44a4aa5be27f6404d785cfc1b2fa8450b6
PaulepaMA <paula.martinez.alvarez@gmail.com>
2017-02-26 13:56:28 +0100

    Add files via upload

---
+++ desktop/src/com/badlogic/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.badlogic.drop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.drop.Gota;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width=800;
+		config.height=480;
+		config.title="Agua";
+		new LwjglApplication(new Gota(), config);
+	}
+}
